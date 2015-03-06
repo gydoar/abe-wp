b3b0dceb2806752635d7a06d9925db22473caeb9
Andres Vega <gydoar@hotmail.com>
2015-03-06 12:50:30 -0500

    agregando page.php

--- page.php
+++ page.php
@@ -1,7 +1,18 @@
 <?php get_header(); ?>
+	
+	<div class="container">
+		<article class="page">
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-	<article class="page">
-		
-	</article>
+			<h3><?php the_title(); ?></h3>
+			<div class="line-niveles2"></div>
+			
+			<?php the_content(); ?>
+
+			<?php endwhile; else : ?>
+				<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+			<?php endif; ?>
+		</article>
+	</div>
 
 <?php get_footer(); ?>
